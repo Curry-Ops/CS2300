b80a6327e0f49f2b047d578dc70ef713645a1c67
Curry-Ops <wheldon79@gmail.com>
2025-03-27 13:43:49 -0400

    Add files via upload

---
+++ DayOfWeek.js
@@ -0,0 +1,9 @@
+<script>
+const day = prompt("Enter the day of the week:");
+
+if (day === "Saturday" || day === "Sunday") {
+    console.log("It's the weekend!");
+} else {
+    console.log("It's a weekday.");
+}
+</script>
